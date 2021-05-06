0229ece67d496a9621bb203b6994a4a113ee3882
homebrew-releaser <homebrew-releaser@example.com>
2021-05-06 12:12:51 +0000

    Brew formula update for Yabai_Wrapper version v1.5.3

--- Formula/Yabai_Wrapper.rb
+++ Formula/Yabai_Wrapper.rb
@@ -5,8 +5,8 @@
 class YabaiWrapper < Formula
   desc "Wrapper for yabai window manager"
   homepage "https://github.com/williampayne23/Yabai_Wrapper"
-  url "https://github.com/williampayne23/Yabai_Wrapper/archive/v1.5.2.tar.gz"
-  sha256 "8d951636255b959b1da62014c84928c1704811b30ef11f5f56f45648279775b8"
+  url "https://github.com/williampayne23/Yabai_Wrapper/archive/v1.5.3.tar.gz"
+  sha256 "0a17306d3c711a76cdaf654157605b482d8f3ca7997f8c83f95430aa71242f2c"
   license "GPL-2.0"
   bottle :unneeded
 
